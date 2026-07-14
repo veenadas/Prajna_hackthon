f456c94df2b2f5b11c95cd3ff85fdd09a1ef8d3c
veenadas <veenadasbs@gmail.com>
2026-07-14 13:29:15 +0530

    Add database connection setup in db.php

---
+++ db.php
@@ -0,0 +1,13 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$password = "";
+$database = "hackthon";
+
+$conn = new mysqli($host, $user, $password, $database);
+
+if ($conn->connect_error) {
+    die("Connection Failed: " . $conn->connect_error);
+}
+?>
